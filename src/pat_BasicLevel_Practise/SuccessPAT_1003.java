4e59ce744729017507f4c70a3b8fe214e65ab559
kangbuu <kangjian_kj@sina.cn>
2018-04-05 12:25:14 +0800

    PAT TEST 3

--- src/pat_BasicLevel_Practise/SuccessPAT_1003.java
+++ src/pat_BasicLevel_Practise/SuccessPAT_1003.java
@@ -10,11 +10,8 @@ import java.util.Scanner;
  * *********************************
  * 得到“答案正确”的条件是：
  * 1. 字符串中必须仅有P, A, T这三种字符，不可以包含其它字符；
- *      ->只能包含 P A T " "
  * 2. 任意形如 xPATx 的字符串都可以获得“答案正确”，其中 x 或者是空字符串，或者是仅由字母 A 组成的字符串；
- *      ->A 可以在 PAT 左右两边
  * 3. 如果 aPbTc 是正确的，那么 aPbATca 也是正确的，其中 a, b, c 均或者是空字符串，或者是仅由字母 A 组成的字符串。
- *      ->a b c要么都是空字符串，要么都是由A 组成的字符串。可以出现在前后或者是 P后
  * 输入格式： 每个测试输入包含1个测试用例。第1行给出一个自然数n (<10)，是需要检测的字符串个数。接下来每个字符串占一行，字符串长度不超过100，且不包含空格。
  *                                                                              -> 不包含空格？
  * 输出格式：每个字符串的检测结果占一行，如果该字符串可以获得“答案正确”，则输出YES，否则输出NO。
@@ -37,6 +34,9 @@ import java.util.Scanner;
  * NO
  * NO
  * NO
+ * ************************
+ * aPbTc
+ * c = b * a
  */
 public class SuccessPAT_1003 {
     public static void main(String[] args) {
@@ -45,7 +45,7 @@ public class SuccessPAT_1003 {
 
         int count = scanner.nextInt();
 
-        String pattern = "[^PAT]";
+        String pattern = "^A{0,}P{1}A{1,}T{1}A{0,}$";
 
         String[] pats = new String[count];
 
