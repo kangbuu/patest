65e59efa74722f41fbb6a30c5a4ce91d016c5904
kangbuu <kangjian_kj@sina.cn>
2018-04-07 11:35:42 +0800

    PAT TEST 1003 真的过分，判断了一下输入字符大于10的情况，就给我一个部分成功。 我完美的逻辑思维都成了负累。。

--- src/pat_BasicLevel_Practise/SuccessPAT_1003.java
+++ src/pat_BasicLevel_Practise/SuccessPAT_1003.java
@@ -1,8 +1,8 @@
 package pat_BasicLevel_Practise;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * @author kangbuu
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * 2. 任意形如 xPATx 的字符串都可以获得“答案正确”，其中 x 或者是空字符串，或者是仅由字母 A 组成的字符串；
  * 3. 如果 aPbTc 是正确的，那么 aPbATca 也是正确的，其中 a, b, c 均或者是空字符串，或者是仅由字母 A 组成的字符串。
  * 输入格式： 每个测试输入包含1个测试用例。第1行给出一个自然数n (<10)，是需要检测的字符串个数。接下来每个字符串占一行，字符串长度不超过100，且不包含空格。
- *                                                                              -> 不包含空格？
+ * -> 不包含空格？ 空字符串不就是空格么？
  * 输出格式：每个字符串的检测结果占一行，如果该字符串可以获得“答案正确”，则输出YES，否则输出NO。
  * 输入样例：
  * 8
@@ -34,31 +34,50 @@ import java.util.Scanner;
  * NO
  * NO
  * NO
- * ************************
+ * **********规律总结**************
  * aPbTc
  * c = b * a
  */
-public class SuccessPAT_1003 {
+
+public class SuccessPAT_1003{
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int count = scanner.nextInt();
 
-        String pattern = "^A{0,}P{1}A{1,}T{1}A{0,}$";
+        String regEx = "^A{0,}P{1}A{1,}T{1}A{0,}$";
 
         String[] pats = new String[count];
+
+        String[] results = new String[count];
+
+        Pattern pattern = Pattern.compile(regEx);
 
         for (int i = 0; i < pats.length; i++) {
 
             pats[i] = scanner.next();
 
-            if (pats[i].contains("P") && pats[i].contains("A") && pats[i].contains("T")){
+            if (pattern.matcher(pats[i]).matches()) {
 
+                int before = pats[i].substring(0, pats[i].indexOf("P")).length();
+
+                int middlePart = pats[i].substring(pats[i].indexOf("P") + 1, pats[i].indexOf("T")).length();
+
+                int after = pats[i].substring(pats[i].indexOf("T") + 1, pats[i].length()).length();
+
+                results[i] = before * middlePart == after ? "YES" : "NO";
+
+            } else {
+
+                results[i] = "NO";
             }
         }
 
+        for (String result : results) {
 
-
+            System.out.println(result);
+        }
     }
 }
